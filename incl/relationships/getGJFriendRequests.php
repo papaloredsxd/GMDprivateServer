35268b17ca61c411b800f5b6afe23f5bb0da3cdf
papaloredsxd <97418565+papaloredsxd@users.noreply.github.com>
2024-09-14 22:39:36 -0400

    Revert "Exploit fix"

--- incl/relationships/getGJFriendRequests.php
+++ incl/relationships/getGJFriendRequests.php
@@ -44,7 +44,11 @@ foreach($result as &$request) {
 	$result2 = $query->fetchAll();
 	$user = $result2[0];
 	$uploadTime = date("d/m/Y G.i", $request["uploadDate"]);
-	$extid = is_numeric($user['extID']) ? $user['extID'] : 0;
+	if(is_numeric($user["extID"])){
+		$extid = $user["extID"];
+	}else{
+		$extid = 0;
+	}
 	$reqstring .= "1:".$user["userName"].":2:".$user["userID"].":9:".$user["icon"].":10:".$user["color1"].":11:".$user["color2"].":14:".$user["iconType"].":15:".$user["special"].":16:".$extid.":32:".$request["ID"].":35:".$request["comment"].":41:".$request["isNew"].":37:".$uploadTime."|";
 
 }
